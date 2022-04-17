c00682d83cfd58e5637f56037c034762cf1df490
BunnyCorruption <huntercharriman@gmail.com>
2022-04-17 19:16:36 -0400

    Spaghetti

--- resources/js/Pages/Jobs/Create.vue
+++ resources/js/Pages/Jobs/Create.vue
@@ -41,13 +41,13 @@
                                                     <tr>
                                                         <th
                                                             scope="col"
-                                                            class="px-6 py-3 text-left text-xs font-medium text-neutral-900 uppercase tracking-wider"
+                                                            class="px-6 py-3 text-left font-medium text-neutral-900 uppercase tracking-wider"
                                                         >
                                                             Series
                                                         </th>
                                                         <th
                                                             scope="col"
-                                                            class="px-6 py-3 text-left text-xs font-medium text-neutral-900 uppercase tracking-wider"
+                                                            class="px-6 py-3 text-left font-medium text-neutral-900 uppercase tracking-wider"
                                                         >
                                                         </th>
                                                     </tr>
